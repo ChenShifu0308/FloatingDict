e0b2e2b4afb077a28668387253d19a637ca43985
Zheng Chen <chen1234zheng1234@gmail.com>
2023-04-19 10:27:59 +0800

    feat: add the setting page

--- app/src/main/java/com/example/floatingdict/MainActivity.kt
+++ app/src/main/java/com/example/floatingdict/MainActivity.kt
@@ -2,10 +2,29 @@ package com.example.floatingdict
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.add
+import androidx.fragment.app.commit
+import androidx.preference.Preference
+import androidx.preference.PreferenceFragmentCompat
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(R.layout.activity_main),
+    PreferenceFragmentCompat.OnPreferenceStartFragmentCallback {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        if (savedInstanceState == null) {
+            supportFragmentManager.commit {
+                setReorderingAllowed(true)
+                add<SettingsFragment>(R.id.fragment_container_view)
+            }
+        }
+        title = "Dev Options"
+    }
+
+    override fun onPreferenceStartFragment(
+        caller: PreferenceFragmentCompat,
+        pref: Preference
+    ): Boolean {
+        // Do nothing now because we don't have any sub-preferences.
+        return true
     }
 }
---
+++ app/src/main/java/com/example/floatingdict/SettingsFragment.kt
@@ -0,0 +1,51 @@
+package com.example.floatingdict
+
+import android.content.Context
+import android.content.SharedPreferences
+import android.os.Bundle
+import android.util.Log
+import androidx.preference.Preference
+import androidx.preference.PreferenceFragmentCompat
+import androidx.preference.PreferenceManager
+import androidx.preference.SwitchPreferenceCompat
+
+class SettingsFragment : PreferenceFragmentCompat() {
+    private val sharedPreferences: SharedPreferences by lazy {
+        requireContext().getSharedPreferences(PREFERENCES_FILE_NAME, Context.MODE_PRIVATE)
+    }
+
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        val manager: PreferenceManager = preferenceManager
+        manager.sharedPreferencesName = PREFERENCES_FILE_NAME
+        setPreferencesFromResource(R.xml.settings, rootKey)
+        setupPreferences()
+    }
+
+    private fun setupPreferences() {
+        val enableSettings: SwitchPreferenceCompat? = findPreference("float_window_enable")
+        enableSettings?.onPreferenceChangeListener =
+            Preference.OnPreferenceChangeListener { preference, newValue ->
+                Log.d("SettingsFragment", "enableSettings newValue: $newValue")
+                if (newValue == true) {
+
+                } else {
+                }
+                true
+            }
+
+        val draggableSettings: SwitchPreferenceCompat? = findPreference("float_window_draggable")
+        draggableSettings?.onPreferenceChangeListener =
+            Preference.OnPreferenceChangeListener { preference, newValue ->
+                Log.d("SettingsFragment", "draggableSettings newValue: $newValue")
+                if (newValue == true) {
+
+                } else {
+                }
+                true
+            }
+    }
+
+    companion object {
+        const val PREFERENCES_FILE_NAME = "settings"
+    }
+}
